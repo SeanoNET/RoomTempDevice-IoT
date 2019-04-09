6f3281a6410ea03feb5e2b5938aed8cf9ba6ecdd
Sean OLoughlin <27134812+SeanoNET@users.noreply.github.com>
2019-04-09 21:34:42 +0800

    Added Sensor init and readings Added error LED util

--- RoomTempDevice/Device/device.ino
+++ RoomTempDevice/Device/device.ino
@@ -1,6 +1,7 @@
 #include "AZ3166WiFi.h"
 #include "DevKitMQTTClient.h"
 #include "RGB_LED.h"
+#include <HTS221Sensor.h>
 
 #define RGB_LED_BRIGHTNESS 32
 
@@ -13,9 +14,20 @@
 #define send_interval 15000
 #define setup_delay 2000
 
+static const float DEFAULT_TEMP = -1000;
+static const float DEFAULT_HUMID = -1000;
+
+DevI2C *i2c;
+HTS221Sensor *sensor;
+
+float humidity = 0;
+float temperature = 0;
+
 static bool hasWifi = false;
 static bool hasIoTHub = false;
+static bool hasSensors = false;
 static RGB_LED rgbLed;
+
 ///// Init
 static void InitWifi(){
   Screen.print(LINE_1, "Connecting WiFi...");
@@ -48,6 +60,21 @@ static void InitIotHub(){
 
 }
 
+static void InitSensors(){
+  Screen.print(LINE_1, "Init Sensors...",true);
+
+  // Setup sensor
+  i2c = new DevI2C(D14, D15);
+  sensor = new HTS221Sensor(*i2c);
+
+  // init sensor
+  sensor->init(NULL);
+
+  humidity = DEFAULT_HUMID;
+  temperature = DEFAULT_TEMP;
+
+  hasSensors = true;
+}
 
 //// MQTT Call backs
 
@@ -61,12 +88,33 @@ static void SendConfirmationCallback(IOTHUB_CLIENT_CONFIRMATION_RESULT result){
 
 //// Util
 
+static void SetLEDError(){
+  rgbLed.setColor(RGB_LED_BRIGHTNESS,0, 0);
+}
+
 static void blinkLED(){
   rgbLed.turnOff();
   rgbLed.setColor(0,0, RGB_LED_BRIGHTNESS);
   delay(500);
   rgbLed.turnOff();
 }
+
+float ReadTemperature(){
+  sensor->reset();
+
+  float temperature = DEFAULT_TEMP;
+  sensor->getTemperature(&temperature);
+  return temperature;
+}
+
+float ReadHumidity(){
+  sensor->reset();
+
+  float humidity = DEFAULT_HUMID;
+  sensor->getHumidity(&humidity);
+  return humidity;
+}
+
 void setup() {
   Screen.print(HEADER, "Setup...");
 
@@ -86,19 +134,27 @@ void setup() {
     return;
   }
 
-  // TODO: Prep Sensors
+  // Init Sensors
+  InitSensors();
+  if(!hasSensors){
+    return;
+  }
 
   Serial.print("Sending interval at ");
   Serial.println(send_interval);
 
   delay(setup_delay);
+
   Screen.clean();
 }
 
 void loop() {
   // put your main code here, to run repeatedly:
-  if (hasIoTHub && hasWifi)
+  if (hasIoTHub && hasWifi && hasSensors)
   {
+    float t = ReadTemperature();
+    float h = ReadHumidity();
+
     char buff[128];
 
     // replace the following line with your data sent to Azure IoTHub
@@ -106,14 +162,22 @@ void loop() {
     
     if (DevKitMQTTClient_SendEvent(buff))
     {
-      Screen.print(LINE_1, "Sending...");
+      Screen.print(HEADER, "Running...");
+      
+      // Update Screen with sensor data
+      char sensorLine[20];
+      sprintf(sensorLine, "T:%.2f H:%.2f", t, h);
+      Screen.print(LINE_3, sensorLine);
+      
     }
     else
     {
       Screen.print(LINE_1, "Failure...");
     }
-    delay(send_interval);
   }else{
     Screen.print(LINE_1, "Error...");
+    SetLEDError();
   }
+
+  delay(send_interval);
 }
